b0a7d384090195ee2d14c5aab17cb95e70867ca7
rosellarade <109802977+Rosellarade@users.noreply.github.com>
2024-07-18 09:07:34 +0800

    screaming and crying

--- Mahallo Web App/src/Pages/Canteen.js
+++ Mahallo Web App/src/Pages/Canteen.js
@@ -7,9 +7,17 @@ function Canteen({goToAlt}){
             <header className='canteen-header'>
               <button className = 'back-button' onClick={goToAlt}>Back</button>
               <div>
-                <h1>canteen</h1>
-                <p> Example of a location</p>
+                <h1>Canteen</h1>
               </div>
+
+            <div class="canteen-bottom-banner">
+              <div className="food-logo"></div>
+
+
+              <div>
+                  <p> © 2024 • SJI Coding & Technology </p>
+              </div>
+            </div>
             </header>
         </div>
     );
